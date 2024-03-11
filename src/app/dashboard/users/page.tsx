bf1d8611065065d197519c70b6c2229b348b7d3e
Jeffrey Fernandez <jeffrey.fernandez.r@outlook.ph>
2024-03-11 08:34:00 +0800

    Use stack in users page

--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 import Snackbar, { SnackbarOrigin } from "@mui/material/Snackbar";
 
 import BasicTable from "../../components/BasicTable";
@@ -19,60 +20,60 @@ const UsersPage: React.FC<any> = () => {
 
   return (
     <Container key={"users-container"}>
-      <Button
-        onClick={() => handlers.updateModalVisibility("admin")}
-        variant="contained"
-      >
-        Create co-admin
-      </Button>
-      <CreateAdminModal
-        isVisible={state.modalVisibility.admin}
-        updateVisibility={handlers.updateModalVisibility}
-        adminUser={state.adminDetails}
-        updateAdminDetails={handlers.updateAdminDetails}
-        addAdmin={handlers.addAdmin}
-      />
-      <BasicTable
-        gutterBottom
-        tableKey="admin-user-table"
-        rowHeaders={["Email", "Name"]}
-        rowData={handlers.formatTableData("admin")}
-      />
+      <Stack spacing={3} direction="column">
+        <Button
+          onClick={() => handlers.updateModalVisibility("admin")}
+          variant="contained"
+        >
+          Create co-admin
+        </Button>
+        <CreateAdminModal
+          isVisible={state.modalVisibility.admin}
+          updateVisibility={handlers.updateModalVisibility}
+          adminUser={state.adminDetails}
+          updateAdminDetails={handlers.updateAdminDetails}
+          addAdmin={handlers.addAdmin}
+        />
+        <BasicTable
+          tableKey="admin-user-table"
+          rowHeaders={["Email", "Name"]}
+          rowData={handlers.formatTableData("admin")}
+        />
 
-      <Button
-        onClick={() => handlers.updateModalVisibility("student")}
-        variant="contained"
-      >
-        Create student
-      </Button>
-      <BasicTable
-        gutterBottom
-        tableKey="student-user-table"
-        rowHeaders={["Email", "Name", "Course"]}
-        rowData={handlers.formatTableData("student")}
-      />
+        <Button
+          onClick={() => handlers.updateModalVisibility("student")}
+          variant="contained"
+        >
+          Create student
+        </Button>
+        <BasicTable
+          tableKey="student-user-table"
+          rowHeaders={["Email", "Name", "Course"]}
+          rowData={handlers.formatTableData("student")}
+        />
 
-      <CreateStudentModal
-        isVisible={state.modalVisibility.student}
-        updateVisibility={handlers.updateModalVisibility}
-        studentUser={state.studentDetails}
-        updateStudentDetails={handlers.updateStudentDetails}
-        addStudent={handlers.addStudent}
-      />
+        <CreateStudentModal
+          isVisible={state.modalVisibility.student}
+          updateVisibility={handlers.updateModalVisibility}
+          studentUser={state.studentDetails}
+          updateStudentDetails={handlers.updateStudentDetails}
+          addStudent={handlers.addStudent}
+        />
 
-      <Snackbar
-        color="primary"
-        anchorOrigin={{
-          horizontal: "center",
-          vertical: "top",
-        }}
-        open={state.snackbar.open}
-        onClose={handlers.handleCloseSnackbar}
-        message={state.snackbar.message}
-        TransitionComponent={state.snackbar.Transition}
-        key={state.snackbar.Transition.name}
-        autoHideDuration={1200}
-      />
+        <Snackbar
+          color="primary"
+          anchorOrigin={{
+            horizontal: "center",
+            vertical: "top",
+          }}
+          open={state.snackbar.open}
+          onClose={handlers.handleCloseSnackbar}
+          message={state.snackbar.message}
+          TransitionComponent={state.snackbar.Transition}
+          key={state.snackbar.Transition.name}
+          autoHideDuration={1200}
+        />
+      </Stack>
     </Container>
   );
 };
